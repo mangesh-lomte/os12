572d22613424843901fe2d8ebdc05b22407edcbf
mangesh-lomte <97304542+mangesh-lomte@users.noreply.github.com>
2022-11-26 00:56:56 +0530

    Add files via upload

---
+++ potter.sh
@@ -0,0 +1,104 @@
+#1/bin/bash
+
+create()
+{
+echo "enter book name"
+read ab
+res='ls | grep $ab } wc -w'
+
+if ( $res > 0);
+then 
+     echo "error"
+else 
+     touch $ab
+     echo "ab created"
+fi     
+}
+Insert()
+{
+            clear
+           
+            eno=$1           
+            echo "Enter Employee No: $eno"
+           
+            echo "Enter Employee Name: \c"
+            read enm
+
+            echo "Enter Employee Address: \c"
+            read eadd
+           
+
+
+           
+            echo "$eno,$enm,$eadd,true" >> Employee.txt 
+
+            echo "                 Insert Sucessfully                           "
+
+}
+Display()
+{
+clear
+      
+for j in `cat Employee.txt `
+do
+eno=$(echo "$j" | cut -d "," -f 1)
+enm=$(echo "$j" | cut -d "," -f 2)
+eadd=$(echo "$j" | cut -d "," -f 3)
+man=$(echo "$j" | cut -d "," -f 4)
+if [ $man = "true" ]
+then
+echo "___________________________________________"
+                                  echo "$eno \t $enm \t\t $eadd"
+fi
+done   
+echo
+}
+Delete()
+{
+            clear
+            f=0
+            echo "Enter Employee NO: \c"
+            read no
+
+            for j in `cat Employee.txt`
+            do
+                        eno=$(echo "$j" | cut -d "," -f 1)
+                        enm=$(echo "$j" | cut -d "," -f 2)
+                        eadd=$(echo "$j" | cut -d "," -f 3)
+                        
+                        if [ $no -eq $eno ]
+                        then
+                                    f=1                              
+                                    line=$(echo "$eno,$enm,$eadd,false")
+                                    fnm=`cat Employee.txt`
+                                    d=$(echo "$fnm" | sed s/$j/$line/g )
+                                    echo $d > Employee.txt          
+                                    echo "                 Delete Successfully                           "
+                        fi
+            done
+            if [ f = 0 ]
+            then
+                          echo "               No Record Found              "
+            fi
+}
+
+            
+while [ true ]
+do 
+echo "1.create"
+echo "2.insert"
+echo "3.Display"
+echo "4.Delete"
+echo "5.exit"
+echo "enter "
+read ch
+
+case $ch in
+ 1)create;;
+ 2)Insert;;
+            3) Display ;;
+            4) Delete ;;
+            5) break ;;
+            *) echo " Wrong Choice "
+esac
+done
